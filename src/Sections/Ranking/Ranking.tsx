9d4f470be8d2bf1114e6ff3788c4e550234475cf
k-karasawa <karasawa@izu-tech.com>
2025-03-15 23:18:25 +0900

    fix-title-margin

--- src/Sections/Ranking/Ranking.tsx
+++ src/Sections/Ranking/Ranking.tsx
@@ -11,7 +11,7 @@ export const Ranking: React.FC = () => {
     >
       <div className="container mx-auto py-24 px-4 md:px-8">
         <motion.div 
-          className="text-center mb-24"
+          className="text-center mb-12 md:mb-24"
           initial={{ opacity: 0, y: 20 }}
           whileInView={{ opacity: 1, y: 0 }}
           viewport={{ once: true }}
